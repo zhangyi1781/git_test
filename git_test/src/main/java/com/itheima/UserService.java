65d0ea620c924c4c80c9b3486d8a3e80d93941ee
zhangsan <zhangsan@itcast.cn>
2019-06-16 23:32:20 +0800

    添加test工程到git

---
+++ git_test/src/main/java/com/itheima/UserService.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class UserService {
+    public static void main(String[] args) {
+
+        System.out.println("hello..");
+        System.out.println("hello..");
+        System.out.println("hello..");
+        System.out.println("hello..");
+        System.out.println("hello..");
+    }
+}
